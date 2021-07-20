28a2c75c21aeb8f9d5b9ffb95a4c80cb9690cbac
Oliver Botello <oliver.botello@profuturo.com.mx>
2021-07-20 14:39:37 -0500

    Constantes comentadas

--- app/src/main/java/com/oliverbotello/movies/utils/Constants.kt
+++ app/src/main/java/com/oliverbotello/movies/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.oliverbotello.movies.utils
 
-const val API_URL = ""
-const val API_KEY = "55957fcf3ba81b137f8fc01ac5a31fb5"
-const val DEFAULT_LANGUAGE = "es"
+//const val API_URL = ""
+//const val API_KEY = "55957fcf3ba81b137f8fc01ac5a31fb5"
+//const val DEFAULT_LANGUAGE = "es"
