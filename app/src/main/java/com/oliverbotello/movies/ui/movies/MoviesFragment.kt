672f4b35db3d692fb706b88700fe01b03de2c83d
Oliver Botello <oliver.botello@profuturo.com.mx>
2021-07-20 14:30:58 -0500

    Libererias

--- app/src/main/java/com/oliverbotello/movies/ui/movies/MoviesFragment.kt
+++ app/src/main/java/com/oliverbotello/movies/ui/movies/MoviesFragment.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.bumptech.glide.Glide
-import com.oliverbotello.movies.MainViewModel
 import com.oliverbotello.movies.R
 import com.oliverbotello.movies.models.Movie
 import com.oliverbotello.movies.ui.adapters.MovieAdapter
